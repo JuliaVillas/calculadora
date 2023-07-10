1ce3c76932d6eaa11afd9da4531ebabac4dee5d8
Julia Villas <juliavbviana03@gmail.com>
2023-07-10 19:58:22 -0300

    Calduladora.py refactored

--- calculadora.py
+++ calculadora.py
@@ -1,32 +1,29 @@
-'''
-CALCULADORA EM PYTHON
+condicao = input('Digite "s" para usar a calculadora, ou "n" para fechar o programa: ')
 
-- O que faz uma calcudora?
-- O que tem uma calculadora?
-    -sinais
-    -memória
-    -números
-'''
+while condicao == 's':  
 
-n1 = float(input('digite um número: '))
-sinal = input('selecione o sinal da operação "+", "-", "*", "/" e "**": ' )
-n2 = float(input('digite outro número: '))
-soma = n1 + n2
-sub = n1 - n2
-mult = n1 * n2
-divi = n1 / n2
-quad = n1 ** n2
+    n1 = float(input('digite um número: '))
+    sinal = input('selecione o sinal da operação "+", "-", "*", "/" e "**": ' )
+    n2 = float(input('digite outro número: '))
+    soma = n1 + n2
+    sub = n1 - n2
+    mult = n1 * n2
+    divi = n1 / n2
+    quad = n1 ** n2
 
-if sinal == '+':
-    print(f'O resultado da adição é: {soma}.')
-elif sinal == '-':
-    print(f'O resultado da subtração é: {sub}.')
-elif sinal == '*':
-    print(f'O resultado da multiplicação é: {mult}')
-elif sinal == '/':
-    print(f'O resultado da divisão é: {divi}')
-elif sinal == '**':
-    print(f'O resultado da divisão é: {quad}')
-else:
-    print('Sinal inválido!')
+    if sinal == '+':
+        print(f'O resultado da adição é: {soma}.')
+    elif sinal == '-':
+        print(f'O resultado da subtração é: {sub}.')
+    elif sinal == '*':
+        print(f'O resultado da multiplicação é: {mult}')
+    elif sinal == '/':
+        print(f'O resultado da divisão é: {divi}')
+    elif sinal == '**':
+        print(f'O resultado da divisão é: {quad}')
+    else:
+        print('Sinal inválido!')
 
+    condicao = input('Digite "s" para usar a calculadora, ou "n" para fechar o programa: ')
+
+print('Fim do programa.')
